e0e225fa43fd7a070b35a795adba070d1f2d1fa3
koto729 <ko.wingto.11@gmail.com>
2023-01-07 18:30:10 +0800

    test 1

--- githubTest/ContentView.swift
+++ githubTest/ContentView.swift
@@ -8,66 +8,23 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
 
+struct WelcomeView: View {
+    @State private var showLogin = false
+ 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem() {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add time", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Soop")
+        VStack {
+            Text("Welcome!")
+                .font(.title)
+     
+            Spacer().frame(height: 20)
+     
+            Button(action: {
+                showLogin = true
+            }, label: {
+                Text("Login")
+            })
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-            
-        }
-        
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-        }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
--- githubTestUITests/githubTestUITests.swift
+++ githubTestUITests/githubTestUITests.swift
@@ -22,12 +22,13 @@ final class githubTestUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testWelcome() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+     
+        let welcome = app.staticTexts["Welcome!"]
+     
+        XCTAssert(welcome.exists)
     }
 
     func testLaunchPerformance() throws {
